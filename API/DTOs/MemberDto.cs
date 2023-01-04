9404d35f4cab4e1e12d5ee3c6e6b88d5c17bbca6
Ibrahim Mohamed <ibrahimgomaa17@gmail.com>
2023-01-04 19:20:55 +0300

    df

--- API/DTOs/MemberDto.cs
+++ API/DTOs/MemberDto.cs
@@ -7,7 +7,7 @@ namespace API.DTOs
 {
     public class MemberDto
     {
-        public int Id { get; set; }
+        public int Id { get; set; } 
         public string  Username { get; set; } 
         public int  Age { get; set; }
         public string  PhotoUrl { get; set; }
